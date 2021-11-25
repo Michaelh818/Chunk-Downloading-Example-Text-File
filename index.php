72aea94bed86727d6dac3660896779c3f43334ff
michaelharootoonyan <harootoonyanmichael@gmail.com>
2021-11-24 18:43:05 -0800

    init

--- index.php
+++ index.php
@@ -1,11 +1,13 @@
 <script>
 var xhr = new XMLHttpRequest();
-let collection = null;
+
+// collection the alphabet in here in 4 chunks
+let collection = "";
 
 xhr.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 206) {
         // 206 partial data
-        console.log(xhr.responseText)
+        // console.log(xhr.responseText)
         collection += xhr.responseText;
         
         
@@ -31,12 +33,12 @@ setTimeout(function(){
 
 setTimeout(function(){
     xhr.open("GET", "api.php", true);
-    xhr.setRequestHeader("Range", "bytes=17-21");
+    xhr.setRequestHeader("Range", "bytes=17-"); // it gets the size of the file in the backend and completes the entire download this way.
     xhr.send();
 }, 3000)
 
-// setTimeout(function(){
-//     console.log(collection);
-// }, 4000)
+setTimeout(function(){
+    console.log(collection);
+}, 4000)
 </script>
 CHECK CONSOLE
